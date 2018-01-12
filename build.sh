827053d1a9d0f61f21fc840fd1502ed376ccf12d
Auston Stewart <auston@shedbuilt.net>
2018-01-12 01:46:37 -0500

    Adding the toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-./configure --prefix=/usr    \
-            --disable-static \
-            --docdir=/usr/share/doc/gettext-0.19.8.1
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-chmod -v 0755 ${SHED_FAKEROOT}/usr/lib/preloadable_libintl.so
+case "$SHED_BUILDMODE" in
+    toolchain)
+        cd gettext-tools
+        EMACS="no" ./configure --prefix=/tools \
+                               --disable-shared || return 1
+        make -C gnulib-lib || return 1
+        make -C intl pluralx.c || return 1
+        make -C src msgfmt || return 1
+        make -C src msgmerge || return 1
+        make -C src xgettext || return 1
+        install -dm755 "${SHED_FAKEROOT}/tools/bin"
+        cp -v src/{msgfmt,msgmerge,xgettext} "${SHED_FAKEROOT}/tools/bin"
+        ;;
+    *)
+        ./configure --prefix=/usr    \
+                    --disable-static \
+                    --docdir=/usr/share/doc/gettext-0.19.8.1 || return 1
+        make -j $SHED_NUMJOBS || return 1
+        make DESTDIR="$SHED_FAKEROOT" install || return 1
+        chmod -v 0755 "${SHED_FAKEROOT}/usr/lib/preloadable_libintl.so"
+        ;;
+esac
