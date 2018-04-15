cf82f9769768a93b5525e2e69a3e9235c9272ae4
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:21:09 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
-        cd gettext-tools
+        cd gettext-tools &&
         EMACS="no" ./configure --prefix=/tools \
-                               --disable-shared || return 1
-        make -C gnulib-lib || return 1
-        make -C intl pluralx.c || return 1
-        make -C src msgfmt || return 1
-        make -C src msgmerge || return 1
-        make -C src xgettext || return 1
-        install -dm755 "${SHED_FAKEROOT}/tools/bin"
-        cp -v src/{msgfmt,msgmerge,xgettext} "${SHED_FAKEROOT}/tools/bin"
+                               --disable-shared &&
+        make -C gnulib-lib &&
+        make -C intl pluralx.c &&
+        make -C src msgfmt &&
+        make -C src msgmerge &&
+        make -C src xgettext &&
+        install -dm755 "${SHED_FAKE_ROOT}/tools/bin" &&
+        cp -v src/{msgfmt,msgmerge,xgettext} "${SHED_FAKE_ROOT}/tools/bin"
         ;;
     *)
+        sed -e '/AppData/N;N;p;s/\.appdata\./.metainfo./' \
+            -i gettext-tools/its/appdata.loc &&
         ./configure --prefix=/usr    \
                     --disable-static \
-                    --docdir=/usr/share/doc/gettext-0.19.8.1 || return 1
-        make -j $SHED_NUMJOBS || return 1
-        make DESTDIR="$SHED_FAKEROOT" install || return 1
-        chmod -v 0755 "${SHED_FAKEROOT}/usr/lib/preloadable_libintl.so"
+                    --docdir=/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION} &&
+        make -j $SHED_NUM_JOBS &&
+        make DESTDIR="$SHED_FAKE_ROOT" install &&
+        chmod -v 0755 "${SHED_FAKE_ROOT}/usr/lib/preloadable_libintl.so"
         ;;
 esac
