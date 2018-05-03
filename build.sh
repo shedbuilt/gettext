893a7141cdd57898ad7fefa95dad803092347be0
Auston Stewart <auston@shedbuilt.net>
2018-05-02 14:48:02 -1000

    Backporting AppData patch from LFS / Blank

--- build.sh
+++ build.sh
@@ -3,21 +3,23 @@ case "$SHED_BUILDMODE" in
     toolchain)
         cd gettext-tools
         EMACS="no" ./configure --prefix=/tools \
-                               --disable-shared || return 1
-        make -C gnulib-lib || return 1
-        make -C intl pluralx.c || return 1
-        make -C src msgfmt || return 1
-        make -C src msgmerge || return 1
-        make -C src xgettext || return 1
-        install -dm755 "${SHED_FAKEROOT}/tools/bin"
+                               --disable-shared &&
+        make -C gnulib-lib &&
+        make -C intl pluralx.c &&
+        make -C src msgfmt &&
+        make -C src msgmerge &&
+        make -C src xgettext &&
+        install -dm755 "${SHED_FAKEROOT}/tools/bin" &&
         cp -v src/{msgfmt,msgmerge,xgettext} "${SHED_FAKEROOT}/tools/bin"
         ;;
     *)
+        sed -e '/AppData/N;N;p;s/\.appdata\./.metainfo./' \
+            -i gettext-tools/its/appdata.loc &&
         ./configure --prefix=/usr    \
                     --disable-static \
-                    --docdir=/usr/share/doc/gettext-0.19.8.1 || return 1
-        make -j $SHED_NUMJOBS || return 1
-        make DESTDIR="$SHED_FAKEROOT" install || return 1
+                    --docdir=/usr/share/doc/gettext-0.19.8.1 &&
+        make -j $SHED_NUMJOBS &&
+        make DESTDIR="$SHED_FAKEROOT" install &&
         chmod -v 0755 "${SHED_FAKEROOT}/usr/lib/preloadable_libintl.so"
         ;;
 esac
